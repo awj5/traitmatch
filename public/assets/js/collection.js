d8b94038d3956317d43a42dfa97ed943a0352a48
Adam Johnson <adam@adamjohnson.org>
2022-07-06 11:59:40 +1000

    Update collection.js

--- public/assets/js/collection.js
+++ public/assets/js/collection.js
@@ -331,7 +331,8 @@ async function loadItem(num, item, date, swapped) {
             toggleOverlay('Wildcard Detected!', 'NFTs you own and also ones with rare 1/1 traits are wildcards that can be matched with any other NFT to receive a special bonus. Wildcards have a <span style="color: #FFFF00;">yellow</span> border when selected.');
         }
 
-        if (window.items.length <= window.gameItemCount - 20) {
+        //if (window.items.length <= window.gameItemCount - 20) {
+        if (num === 20 || num === window.items.length) {
             document.querySelector('#game-controls').classList.add('enabled');
         }
 
